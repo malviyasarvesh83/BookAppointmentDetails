7e5a95b29098dc49b8d1244abe02a718b5523796
malviyasarvesh83 <malviyasarvesh83@gmail.com>
2023-01-30 14:59:07 +0530

    Second Commit

--- script.js
+++ script.js
@@ -1,150 +1,193 @@
 function validateForm() {
-  var name = document.getElementById('name').value;
-  var email = document.getElementById('email').value;
-  var phone = document.getElementById('phone').value;
-  var time = document.getElementById('time').value;
+  let name = document.getElementById('name').value;
+  let email = document.getElementById('email').value;
+  let phone = document.getElementById('phone').value;
+  let date = document.getElementById('date').value;
+  let time = document.getElementById('time').value;
 
   if (name == "") {
-    alert("Name is required");
+    alert('Name is required');
     return false;
   }
   if (email == "") {
-    alert("Email is required");
+    alert('Email is required');
     return false;
   }
   if (phone == "") {
-    alert("Phone is required");
+    alert('Phone Number is required');
+    return false;
+  }
+  if (date == "") {
+    alert('Date is required');
     return false;
   }
   if (time == "") {
-    alert("Date & Time is required");
+    alert('Time is required');
     return false;
   }
   return true;
 }
 
-// Function to Show Data
 
-function showData() {
-  var appointmentList;
-  if (localStorage.getItem("appointmentList") == null) {
-    appointmentList = [];
-  } else {
-    appointmentList = JSON.parse(localStorage.getItem("appointmentList"));
+// Function to show data
+
+function showData(response) {
+  let html = "";
+
+  for (let i = 0; i < response.data.length; i++) {
+    html = html + "<tr>";
+    html = html + "<td>" + response.data[i].name + "</td>";
+    html = html + "<td>" + response.data[i].email + "</td>";
+    html = html + "<td>" + response.data[i].phone + "</td>";
+    html = html + "<td>" + response.data[i].date + "</td>";
+    html = html + "<td>" + response.data[i].time + "</td>";
+    html = html + '<td><button class="btn btn-danger deleteBtn" value=' +response.data[i]._id+ '>Delete</button><button class="btn btn-warning m-2 updateBtn" value=' +response.data[i]._id+ '>Edit</button></td>';
+    html = html + "</tr>";
   }
-
-  var html = "";
-
-  appointmentList.forEach(function (ele, index) {
-    html += "<tr>";
-    html += "<td>" + ele.name + "</td>";
-    html += "<td>" + ele.email + "</td>";
-    html += "<td>" + ele.phone + "</td>";
-    html += "<td>" + ele.time + "</td>";
-    html +=
-      '<td><button onclick="deleteData(' +
-      index +
-      ')" class="btn btn-danger">Delete</button><button onclick="updateData(' +
-      index +
-      ')" class="btn btn-warning ml-2">Edit</button></td>';
-    html += "</tr>";
-  });
-
-  document.querySelector('#crudTable tbody').innerHTML = html;
+  document.querySelector("#crudTable tbody").innerHTML = html;
 }
 
-// Show Data On Page Load
+// Get Api
 
-document.onload = showData();
+axios.get('https://crudcrud.com/api/a172cdead9c449fc8c2e4e5884e944d7/appointmentData')
+.then((response)=>{
+  showData(response);
+}).catch((err) => {
+  console.log(err);
+})
 
-// Function to Add Data
+// Function to Add/Post Data
 
-function addData() {
-  console.log('Book Appointent Clicked...!')
+async function addData() {
+  console.log('entered in addData');
   if (validateForm() == true) {
-    var name = document.getElementById("name").value;
-    var email = document.getElementById("email").value;
-    var phone = document.getElementById("phone").value;
-    var time = document.getElementById("time").value;
+    let name = document.getElementById('name').value;
+    let email = document.getElementById('email').value;
+    let phone = document.getElementById('phone').value;
+    let date = document.getElementById('date').value;
+    let time = document.getElementById('time').value;
+    
+    await axios.post('https://crudcrud.com/api/a172cdead9c449fc8c2e4e5884e944d7/appointmentData', { "name": name, "email":email, "phone": phone, "date": date, "time":time })
+    .then((response)=>{
+      showData(response);
+    }).catch((err) => {
+      console.log(err);
+    })
+    console.log('entered After Axios..')
 
-    var appointmentList;
-    if (localStorage.getItem("appointmentList") == null) {
-      appointmentList = [];
-    } else {
-      appointmentList = JSON.parse(localStorage.getItem("appointmentList"));
-    }
+    document.getElementById('name').value = "";
+    document.getElementById('email').value = "";
+    document.getElementById('phone').value = "";
+    document.getElementById('date').value = "";
+    document.getElementById('time').value = "";
 
-    appointmentList.push({
-      name: name,
-      email: email,
-      phone: phone,
-      time: time,
-    });
+    // Get Api
 
-    localStorage.setItem("appointmentList", JSON.stringify(appointmentList));
-    showData();
-    document.getElementById("name").value = "";
-    document.getElementById("email").value = "";
-    document.getElementById("phone").value = "";
-    document.getElementById("time").value = "";
+    await axios.get('https://crudcrud.com/api/a172cdead9c449fc8c2e4e5884e944d7/appointmentData')
+    .then((response)=>{
+      showData(response);
+    }).catch((err) => {
+      console.log(err);
+    })
   }
 }
 
 // Function to Delete Data
 
-function deleteData(index) {
-  var appointmentList;
-  if (localStorage.getItem("appointmentList") == null) {
-    appointmentList = [];
-  } else {
-    appointmentList = JSON.parse(localStorage.getItem("appointmentList"));
-  }
+$("body").on("click", ".deleteBtn", async function () {
+  let id = $(this).val();
+  await axios.delete('https://crudcrud.com/api/a172cdead9c449fc8c2e4e5884e944d7/appointmentData' + '/' + id).then((response) => {
+    console.log(response);
+  }).catch((err) => {
+    console.log(err);
+  })
 
-  appointmentList.splice(index, 1);
-  localStorage.setItem("appointmentList", JSON.stringify(appointmentList));
-  showData();
-}
+  // Get Api
+
+  await axios.get('https://crudcrud.com/api/a172cdead9c449fc8c2e4e5884e944d7/appointmentData')
+  .then((response)=>{
+    showData(response);
+  }).catch((err) => {
+    console.log(err);
+  })
+  
+});
 
 // Function to Update/Edit Data
 
-function updateData(index) {
-  
-  // Submit button will hide and update button will show
+$("body").on("click", ".updateBtn", async function () {
+  // Submit button will hide and Update button will show while clicking on edit button
+  document.getElementById("Submit").style.display = "none";
+  document.getElementById("Update").style.display = "block";
 
-  document.getElementById('Submit').style.display = 'none';
-  document.getElementById('Update').style.display = 'block';
+  let id = $(this).val();
+  await axios
+    .get(
+      "https://crudcrud.com/api/a172cdead9c449fc8c2e4e5884e944d7/appointmentData" +
+        "/" +
+        id
+    )
+    .then((response) => {
+      document.getElementById("name").value = response.data.name;
+      document.getElementById("email").value = response.data.email;
+      document.getElementById("phone").value = response.data.phone;
+      document.getElementById("date").value = response.data.date;
+      document.getElementById("time").value = response.data.time;
 
-  var appointmentList;
-  if (localStorage.getItem("appointmentList") == null) {
-    appointmentList = [];
-  } else {
-    appointmentList = JSON.parse(localStorage.getItem("appointmentList"));
-  }
+      document.querySelector("#Update").onclick = async function () {
+        if (validateForm() == true) {
+          let name = (response.data.name =
+            document.getElementById("name").value);
+          let email = (response.data.email =
+            document.getElementById("email").value);
+          let phone = (response.data.phone =
+            document.getElementById("phone").value);
+          let date = (response.data.date =
+            document.getElementById("date").value);
+          let time = (response.data.time =
+            document.getElementById("time").value);
 
-  document.getElementById('name').value = appointmentList[index].name;
-  document.getElementById('email').value = appointmentList[index].email;
-  document.getElementById('phone').value = appointmentList[index].phone;
-  document.getElementById('time').value = appointmentList[index].time;
+          await axios
+            .put(
+              "https://crudcrud.com/api/a172cdead9c449fc8c2e4e5884e944d7/appointmentData" +
+                "/" +
+                id,
+              { name: name, email: email, phone: phone, date: date, time: time }
+            )
+            .then((response) => {
+              console.log(response);
+            })
+            .catch((err) => {
+              console.log(err);
+            });
+          
+          document.getElementById("name").value = "";
+          document.getElementById("email").value = "";
+          document.getElementById("phone").value = "";
+          document.getElementById("date").value = "";
+          document.getElementById("time").value = "";
 
-  document.querySelector('#Update').onclick = function () {
-    if (validateForm() == true) {
-      appointmentList[index].name = document.getElementById("name").value;
-      appointmentList[index].email = document.getElementById("email").value;
-      appointmentList[index].phone = document.getElementById("phone").value;
-      appointmentList[index].time = document.getElementById("time").value;
+          // Update button will hide and Submit button will show after Updating details
+          document.getElementById("Submit").style.display = "block";
+          document.getElementById("Update").style.display = "none";
 
-      localStorage.setItem("appointmentList", JSON.stringify(appointmentList));
-      showData();
+          // Get Api
 
-      document.getElementById("name").value = "";
-      document.getElementById("email").value = "";
-      document.getElementById("phone").value = "";
-      document.getElementById("time").value = "";
+          await axios
+            .get(
+              "https://crudcrud.com/api/a172cdead9c449fc8c2e4e5884e944d7/appointmentData"
+            )
+            .then((response) => {
+              showData(response);
+            })
+            .catch((err) => {
+              console.log(err);
+            });
+        }
+      };
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
-      // Update button will hide and Submit button will show
-
-      document.getElementById("Submit").style.display = "block";
-      document.getElementById("Update").style.display = "none";
-    }
-  }
-}
